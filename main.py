53d30c2b862a4c9561c344fefbcb8fe4f676bf39
Nikita Osipov <osipov_nik00@mail.ru>
2024-04-15 15:46:57 +0300

    object detection and generation models test

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import pickle
+import numpy as np
+from PIL import Image
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 import streamlit as st
 import streamlit_authenticator as stauth
+
+import torch
+from torchvision.utils import draw_bounding_boxes
+from torchvision.models.detection import (
+    fasterrcnn_resnet50_fpn_v2,
+    FasterRCNN_ResNet50_FPN_V2_Weights,
+)
+
+from transformers import (
+    # AutoTokenizer,
+    # AutoModelForCausalLM,
+    # BitsAndBytesConfig,
+    # GPT2LMHeadModel,
+    # GPT2Tokenizer,
+    T5Tokenizer,
+    T5ForConditionalGeneration,
+)
 
 # --- SETTING CONFIGURATION ---
 st.set_page_config(page_title="Recipes Project")
@@ -27,6 +47,98 @@ authenticator = stauth.Authenticate(
 
 name, authenticaton_status, username = authenticator.login("Login", "main")
 
+# --- DETECTION MODEL  ---
+weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+categories = weights.meta["categories"]
+img_preprocess = weights.transforms()
+
+
+@st.cache_resource
+def load_model():
+    model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.5)
+    model.eval()
+
+    return model
+
+
+obj_model = load_model()
+
+
+def make_prediction(img):
+    img_processed = img_preprocess(img)
+    prediction = obj_model(img_processed.unsqueeze(0))
+    prediction = prediction[0]
+    prediction["labels"] = [categories[label] for label in prediction["labels"]]
+    return prediction
+
+
+def create_image_with_bboxes(img, prediction):
+    img_tensor = torch.tensor(img)
+    img_with_bboxes = draw_bounding_boxes(
+        img_tensor,
+        boxes=prediction["boxes"],
+        labels=prediction["labels"],
+        colors=[
+            "red" if label == "person" else "green" for label in prediction["labels"]
+        ],
+        width=2,
+    )
+    img_with_bboxes_np = img_with_bboxes.detach().numpy().transpose(1, 2, 0)
+    return img_with_bboxes_np
+
+
+# --- GENERATIVE MODEL  ---
+# bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+
+# model_id = "CohereForAI/c4ai-command-r-plus"
+# tokenizer = AutoTokenizer.from_pretrained(model_id)
+# gen_model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config)
+
+# def generate_recipe(ingredients):
+#     messages = [{"role": "user", "content": "Hello, how are you?"}]
+#     input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+
+#     gen_tokens = gen_model.generate(
+#         input_ids,
+#         max_new_tokens=100,
+#         do_sample=True,
+#         temperature=0.3,
+#         )
+
+#     recipe = tokenizer.decode(gen_tokens[0])
+
+#     return recipe
+
+# gen_model = GPT2LMHeadModel.from_pretrained("gpt2")
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+# def generate_recipe(ingredients):
+#     input_text = "Ingredients:" + ", ".join(ingredients) + "\nInstructions:"
+#     input_ids = tokenizer.encode(input_text, return_tensors="pt")
+
+#     output = gen_model.generate(input_ids, max_length=200, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+
+#     recipe = tokenizer.decode(output[0], skip_special_tokens=True)
+
+#     return recipe
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+gen_model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
+
+
+def generate_recipe(ingredients):
+    input_text = (
+        "Provide posibles recipes (at least 3 different options) and step-by-step instructions how to make them with following ingredients:"
+        + ", ".join(ingredients)
+    )
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+    outputs = gen_model.generate(input_ids, max_new_tokens=200)
+    recipe = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return recipe
+
+
 if authenticaton_status is False:
     st.error("Username/password is incorrect")
 if authenticaton_status is None:
@@ -34,3 +146,32 @@ if authenticaton_status is None:
 if authenticaton_status:
     authenticator.logout("Logout", "sidebar")
     st.sidebar.title(f"Welcome {name}")
+
+    st.title("Object Detector :tea: :coffee:")
+    upload = st.file_uploader(label="Upload Image Here:", type=["png", "jpg", "jpeg"])
+
+    if upload:
+        img = Image.open(upload)
+
+        prediction = make_prediction(img)
+        img_with_bbox = create_image_with_bboxes(
+            np.array(img).transpose(2, 0, 1), prediction
+        )
+
+        fig = plt.figure(figsize=(12, 12))
+        ax = fig.add_subplot(111)
+        plt.imshow(img_with_bbox)
+        plt.xticks([], [])
+        plt.yticks([], [])
+        ax.spines[["top", "bottom", "right", "left"]].set_visible(False)
+
+        st.pyplot(fig, use_container_width=True)
+
+        del prediction["boxes"]
+        st.header("Predicted Probabilities")
+        st.write(prediction)
+
+        ingredients = prediction["labels"]
+        ingredients = ["tomato", "cheese", "pasta"]
+        recipe = generate_recipe(ingredients)
+        st.write(recipe)
